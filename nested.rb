0f8c78b9af3dea0edcc490f7049ea0505f0d7069
Maharaj Syed <maharajsyed@yahoo.com>
2020-03-03 22:21:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash [:dennis_ritchies_language][:languages]
+programmer_hash [:dennis_ritchies_language][:languages][0]
 end
 
 def adding_matz
